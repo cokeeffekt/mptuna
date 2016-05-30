d316c81a19ca048e0df7ef7f6797b111fbf15e6e
Patrik Hartwig <patrik.hartwig@megaport.com>
2016-05-30 17:10:45 +1000

    :| music n shit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # mptuna :|
+
+no html bodys yet just testing but this streams musics...
+
+localhost:3000/index.html < is the client
+
+localhost:3000/server.html < is the server
--- app/init.js
+++ app/init.js
@@ -1,4 +1,24 @@
-// peerjs rx1g1udef1ypsyvi
+var peer = new Peer({
+  key: 'rx1g1udef1ypsyvi'
+});
+
+peer.on('open', function (id) {
+  console.log('My peer ID is: ' + id);
+
+  // ping the server to give peerid, it will respond with music.
+  var conn = peer.connect('megaport-tuna-server');
+
+  peer.on('call', function (call) {
+    console.log('server is trying to give you music.');
+    call.answer();
+    call.on('stream', function (stream) {
+      console.log('STREAMING!!!!');
+      $('#player').prop('src', URL.createObjectURL(stream));
+    });
+  });
+
+});
+
 var mpApp = new Vue({
   data: {}
 });
